4e369ad779e1d60ae4b788779f679b8dbb3b2872
csimmons2024 <tul13214@temple.edu>
2024-08-30 11:47:57 -0400

    Total Amount

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
+#include <stdio.h>
+#include <math.h>
 
+int getInfectedAfterDays(int days) {
+    int intInf = 7;
+    float rate = 1.2;
+
+    if (days == 0) {
+        return intInf;
+    }
+
+    return trunc(rate * getInfectedAfterDays (days-1) + 1);
+}
 
 int main() {
 //
-
-}
-
-int getInfected(int days) {
 
 }
