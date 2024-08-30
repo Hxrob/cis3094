accd2950fa2abc23315399f0ddbf266c59c69c1f
Riviera Sperduto <52908660+rivie13@users.noreply.github.com>
2024-08-30 11:46:53 -0400

    Update main.c
    added populationInfected function

--- main.c
+++ main.c
@@ -8,3 +8,10 @@ int main() {
 int getInfected(int days) {
 
 }
+
+int populationInfected(int infectedNumber, int popNumber )
+{
+   int percentInfected =  ( popNumber / infectedNumber ) * 100;
+
+   return percentInfected;
+}
